1cc5103aac7975bcda97e7e2937ba2164873b6ad
kowalikw <kowalikw.mini@gmail.com>
2015-12-03 13:33:13 +0100

    Antialiasing Xiaolin Wu.

--- src/Main/Utilities/DrawHelper.cs
+++ src/Main/Utilities/DrawHelper.cs
@@ -111,20 +111,27 @@ namespace SRL.Main.Utilities
         internal static void DrawLine(this SpriteBatch spriteBatch, Point point1, Point point2, Color color, float thickness = 1.0f)
         {
             // calculate the distance between the two vectors
-            float length = Vector2.Distance(new Vector2((float)point1.X, (float)point1.Y), new Vector2((float)point2.X, (float)point2.Y));
+            //float length = Vector2.Distance(new Vector2((float)point1.X, (float)point1.Y), new Vector2((float)point2.X, (float)point2.Y));
 
             // calculate the angle between the two vectors
-            float angle = (float)Math.Atan2(point2.Y - point1.Y, point2.X - point1.X);
+            //float angle = (float)Math.Atan2(point2.Y - point1.Y, point2.X - point1.X);
+
+            
 
             //DrawLine(spriteBatch, point1, distance, angle, color, thickness);
 
             if (_pixel == null)
-            {
                 CreateThePixel(spriteBatch);
-            }
+
+            /*if (color.R == 0 && color.G == 0 && color.B == 0)
+                _pixel.SetData(new[] { Color.White });
+            else if(color.R == 255 && color.G == 255 && color.B == 255)
+                _pixel.SetData(new[] { Color.White });
+            else if (color.R == 255 || color.G == 255 || color.B == 255)
+                _pixel.SetData(new[] { Color.White });*/
 
             // stretch the pixel between the two vectors
-            spriteBatch.Draw(_pixel,
+            /*spriteBatch.Draw(_pixel,
                              new Vector2((float)point1.X, (float)point1.Y),
                              null,
                              color,
@@ -132,7 +139,176 @@ namespace SRL.Main.Utilities
                              Vector2.Zero,
                              new Vector2(length, thickness),
                              SpriteEffects.None,
-                             0);
+                             0);*/
+
+            double x0 = point1.X;
+            double y0 = point1.Y;
+            double x1 = point2.X;
+            double y1 = point2.Y;
+
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+
+            
+
+            if (steep)
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+
+            if(x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+
+            //Console.WriteLine($"x={x0}, y0={y0}, x1={x1}, y1={y1}, steep={steep}");
+
+            double dx = x1 - x0;
+            double dy = y1 - y0;
+            double gradient = dy / dx;
+
+            // handle first endpoint
+            double xEnd = Math.Round(x0);
+            double yEnd = y0 + gradient * (xEnd - x0);
+            double xGap = rfpart(x0 + 0.5);
+            int xPxl1 = (int)xEnd;
+            int yPxl1 = (int)yEnd;
+
+            if(steep)
+            {
+                spriteBatch.PutPixel(yPxl1, xPxl1, color, (float)(rfpart(yEnd) * xGap));
+                spriteBatch.PutPixel(yPxl1 + 1, xPxl1, color, (float)(fpart(yEnd) * xGap));
+            }
+            else
+            {
+                spriteBatch.PutPixel(xPxl1, yPxl1, color, (float)(rfpart(yEnd) * xGap));
+                spriteBatch.PutPixel(xPxl1, yPxl1 + 1, color, (float)(fpart(yEnd) * xGap));
+            }
+
+            double intery = yEnd + gradient;
+
+            // handle second endpoint
+            xEnd = Math.Round(x1);
+            yEnd = y1 + gradient * (xEnd - x1);
+            xGap = fpart(x1 + 0.5);
+            int xPxl2 = (int)xEnd;
+            int yPxl2 = (int)yEnd;
+
+            if (steep)
+            {
+                spriteBatch.PutPixel(yPxl2, xPxl2, color, (float)(rfpart(yEnd) * xGap));
+                spriteBatch.PutPixel(yPxl2 + 1, xPxl2, color, (float)(fpart(yEnd) * xGap));
+            }
+            else
+            {
+                spriteBatch.PutPixel(xPxl2, yPxl2, color, (float)(rfpart(yEnd) * xGap));
+                spriteBatch.PutPixel(xPxl2, yPxl2 + 1, color, (float)(fpart(yEnd) * xGap));
+            }
+
+            // main loop
+            for(int x = xPxl1 + 1; x < xPxl2; x++)
+            {
+                if(steep)
+                {
+                    spriteBatch.PutPixel((int)intery, x, color, rfpart(intery));
+                    spriteBatch.PutPixel((int)intery + 1, x, color, fpart(intery));
+                }
+                else
+                {
+                    spriteBatch.PutPixel(x, (int)intery, color, rfpart(intery));
+                    spriteBatch.PutPixel(x, (int)intery + 1, color, fpart(intery));
+                }
+                intery = intery + gradient;
+            }
+
+
+            /*int bWidth = 512;
+            int bHeight = 512;
+
+            int x1 = (int)point1.X;
+            int y1 = (int)point1.Y;
+            int x2 = (int)point2.X;
+            int y2 = (int)point2.Y;
+            // zmienne pomocnicze
+            int d, dx, dy, ai, bi, xi, yi;
+            int x = x1, y = y1;
+            // ustalenie kierunku rysowania
+            if (x1 < x2)
+            {
+                xi = 1;
+                dx = x2 - x1;
+            }
+            else
+            {
+                xi = -1;
+                dx = x1 - x2;
+            }
+            // ustalenie kierunku rysowania
+            if (y1 < y2)
+            {
+                yi = 1;
+                dy = y2 - y1;
+            }
+            else
+            {
+                yi = -1;
+                dy = y1 - y2;
+            }
+            // pierwszy piksel
+            if (x < bWidth && x >= 0 && y < bHeight && y >= 0)
+                spriteBatch.PutPixel(x, y, color);
+            // oś wiodąca OX
+            if (dx > dy)
+            {
+                ai = (dy - dx) * 2;
+                bi = dy * 2;
+                d = bi - dx;
+                // pętla po kolejnych x
+                while (x != x2)
+                {
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        x += xi;
+                    }
+                    if (x < bWidth && x >= 0 && y < bHeight && y >= 0)
+                        spriteBatch.PutPixel(x, y, color);
+                }
+            }
+            // oś wiodąca OY
+            else
+            {
+                ai = (dx - dy) * 2;
+                bi = dx * 2;
+                d = bi - dy;
+                // pętla po kolejnych y
+                while (y != y2)
+                {
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        y += yi;
+                    }
+                    if (x < bWidth && x >= 0 && y < bHeight && y >= 0)
+                        spriteBatch.PutPixel(x, y, color);
+                }
+            }*/
+
         }
 
         /// <summary>
@@ -152,7 +328,7 @@ namespace SRL.Main.Utilities
             }
 
             // stretch the pixel between the two vectors
-            spriteBatch.Draw(_pixel,
+            /*spriteBatch.Draw(_pixel,
                              new Vector2((float)point.X, (float)point.Y),
                              null,
                              color,
@@ -160,7 +336,21 @@ namespace SRL.Main.Utilities
                              Vector2.Zero,
                              new Vector2(length, thickness),
                              SpriteEffects.None,
-                             0);
+                             0);*/
+        }
+
+        internal static void PutPixel(this SpriteBatch spriteBatch, int x, int y, Color color, float intense)
+        {
+            Color newColor = new Color((float)color.R * intense/ 255.0f, (float)color.G * intense / 255.0f, (float)color.B * intense / 255.0f, intense);
+            PutPixel(spriteBatch, new Point(x, y), newColor);
+            PutPixel(spriteBatch, new Point(x + 1, y), newColor);
+            PutPixel(spriteBatch, new Point(x, y + 1), newColor);
+            PutPixel(spriteBatch, new Point(x + 1, y + 1), newColor);
+        }
+
+        internal static void PutPixel(this SpriteBatch spriteBatch, int x, int y, Color color)
+        {
+            PutPixel(spriteBatch, new Point(x, y), color);
         }
 
         /// <summary>
@@ -171,10 +361,7 @@ namespace SRL.Main.Utilities
         /// <param name="color">The color of the pixel.</param>
         internal static void PutPixel(this SpriteBatch spriteBatch, Point position, Color color)
         {
-            if (_pixel == null)
-            {
-                CreateThePixel(spriteBatch);
-            }
+           
 
             spriteBatch.Draw(_pixel, new Vector2((float)position.X, (float)position.Y), color);
         }
@@ -196,10 +383,17 @@ namespace SRL.Main.Utilities
         internal static void DrawPath(this SpriteBatch spriteBatch, List<Point> vertices, bool closed, Color color, float thickness = 1.0f)
         {
             for (int i = 0; i < vertices.Count - 1; i++)
+            {
                 spriteBatch.DrawLine(vertices[i], vertices[i + 1], color, thickness);
+                //spriteBatch.DrawLine(new Point(vertices[i].X, vertices[i].Y + 1), new Point(vertices[i + 1].X, vertices[i + 1].Y + 1), color, thickness);
+                //spriteBatch.DrawLine(new Point(vertices[i].X + 1, vertices[i].Y), new Point(vertices[i + 1].X + 1, vertices[i + 1].Y), color, thickness);
+            }
 
             if (closed)
+            {
                 spriteBatch.DrawLine(vertices[vertices.Count - 1], vertices[0], color, thickness);
+                //spriteBatch.DrawLine(new Point(vertices[vertices.Count - 1].X, vertices[vertices.Count - 1].Y + 1), new Point(vertices[0].X, vertices[0].Y + 1), color, thickness);
+            }
         }
 
         internal static void DrawPolygon(this SpriteBatch spriteBatch, Polygon polygon, Color color, float thickness = 1.0f)
@@ -258,6 +452,30 @@ namespace SRL.Main.Utilities
             const int circleSides = 10;
 
             spriteBatch.DrawCircle(vertex, circleRadius, circleSides, color, thickness);
+        }
+
+        private static void Swap(ref double x, ref double y)
+        {
+            double temp = x;
+            x = y;
+            y = temp;
+        }
+
+        private static int ipart(double val)
+        {
+            return (int)val;
+        }
+
+        private static float fpart(double val)
+        {
+            if (val < 0)
+                return (float)(1 - (val - Math.Floor(val)));
+            return (float)(val - Math.Floor(val));
+        }
+
+        private static float rfpart(double val)
+        {
+            return 1 - fpart(val);
         }
     }
 }
