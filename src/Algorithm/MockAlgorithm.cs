75462d063c20edac0457280fd79830bcf4b58f9e
Matt Pielat <mateusz.pielat@gmail.com>
2015-12-04 21:30:17 +0100

    Algorithm project.

---
+++ src/Algorithm/MockAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using SRL.Model;
+using SRL.Model.Model;
+
+namespace SRL.Algorithm
+{
+    public class MockAlgorithm : IAlgorithm
+    {
+        public List<Order> GetPath(Map map, Vehicle vehicle, Point start, Point end, Point vehicleRotation)
+        {
+            //TODO
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
