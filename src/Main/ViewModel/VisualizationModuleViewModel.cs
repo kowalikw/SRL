8d827b8924de24dab55a957c1806dd68a4ab91eb
Matt Pielat <mateusz.pielat@gmail.com>
2015-12-12 21:00:37 +0100

    Frame generation progress (somewhat tested).

--- src/Main/ViewModel/VisualizationModuleViewModel.cs
+++ src/Main/ViewModel/VisualizationModuleViewModel.cs
@@ -48,14 +48,14 @@ namespace SRL.Main.ViewModel
         {
             const int partsPerRadian = 128;
             double radiansPerPart = 1 / (double)partsPerRadian;
-            
+
             Stack<Frame> frames = new Stack<Frame>();
             frames.Push(new Frame
             {
                 Position = Startpoint,
                 Rotation = InitialRotation
             });
-            
+
             for (int o = 0; o < orders.Count; o++)
             {
                 // Rotation frames.
@@ -75,7 +75,7 @@ namespace SRL.Main.ViewModel
                 for (int p = 0; p < partCount - 1; p++)
                 {
                     currentAngle += angleChange;
-                    
+
                     frames.Push(new Frame
                     {
                         Position = currentPosition,
@@ -96,11 +96,14 @@ namespace SRL.Main.ViewModel
                 double dx = end.X - start.X;
                 double dy = end.Y - start.Y;
 
+                double xStep = dx > 0 ? 1 : -1;
+                double yStep = dy > 0 ? 1 : -1;
+
                 currentAngle = frames.Peek().Rotation;
                 currentPosition = frames.Peek().Position;
                 if (Math.Abs(dx) < 0.5)
                 {
-                    for (double y = 1; y <= dy; y++)
+                    for (double y = yStep; Math.Abs(y) <= Math.Abs(dy); y += yStep)
                     {
                         frames.Push(new Frame
                         {
@@ -111,7 +114,7 @@ namespace SRL.Main.ViewModel
                 }
                 else if (Math.Abs(dy) < 0.5)
                 {
-                    for (double x = 1; x <= dx; x++)
+                    for (double x = xStep; Math.Abs(x) <= Math.Abs(dx); x += yStep)
                     {
                         frames.Push(new Frame
                         {
@@ -122,22 +125,18 @@ namespace SRL.Main.ViewModel
                 }
                 else
                 {
-                    double xStep = dx > 0 ? 1 : -1;
-                    double yStep = dy > 0 ? 1 : -1;
-
                     // Based on Bresenham's line algorithm.
                     if (Math.Abs(dx) > Math.Abs(dy))
                     {
-                        double error = Math.Abs(dx/2) - Math.Abs(dy);
-                        for (double x = xStep, y = 0; x <= dx; x += xStep)
+                        double error = Math.Abs(dx / 2) - Math.Abs(dy);
+                        for (double x = xStep, y = 0; Math.Abs(x) <= Math.Abs(dx); x += xStep)
                         {
                             if (error < 0)
                             {
                                 y += yStep;
                                 error += Math.Abs(dx);
                             }
-                            else
-                                error -= Math.Abs(dy);
+                            error -= Math.Abs(dy);
 
                             frames.Push(new Frame
                             {
@@ -149,15 +148,14 @@ namespace SRL.Main.ViewModel
                     else
                     {
                         double error = Math.Abs(dy / 2) - Math.Abs(dx);
-                        for (double y = yStep, x = 0; y <= dx; y += yStep)
+                        for (double y = yStep, x = 0; Math.Abs(y) <= Math.Abs(dy); y += yStep)
                         {
                             if (error < 0)
                             {
                                 x += xStep;
                                 error += Math.Abs(dy);
                             }
-                            else
-                                error -= Math.Abs(dx);
+                            error -= Math.Abs(dx);
 
                             frames.Push(new Frame
                             {
@@ -170,7 +168,7 @@ namespace SRL.Main.ViewModel
 
             }
             _frames = frames.ToArray();
-            _frames = (Frame[])_frames.Reverse();
+            Array.Reverse(_frames);
         }
     }
 }
