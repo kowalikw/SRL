f9a6aae1bedbbea7435e78b61fedba06cddce848
Matt Pielat <mateusz.pielat@gmail.com>
2015-12-12 16:55:48 +0100

    DivideIntoFrames() done but not tested.

--- src/Main/ViewModel/VisualizationModuleViewModel.cs
+++ src/Main/ViewModel/VisualizationModuleViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using SRL.Model;
 using SRL.Model.Model;
@@ -46,7 +47,7 @@ namespace SRL.Main.ViewModel
         private void DivideIntoFrames(List<Order> orders)
         {
             const int partsPerRadian = 128;
-            double radiansPerPart = 1 / partsPerRadian;
+            double radiansPerPart = 1 / (double)partsPerRadian;
             
             Stack<Frame> frames = new Stack<Frame>();
             frames.Push(new Frame
@@ -54,27 +55,16 @@ namespace SRL.Main.ViewModel
                 Position = Startpoint,
                 Rotation = InitialRotation
             });
-
+            
             for (int o = 0; o < orders.Count; o++)
             {
-                // rotation
+                // Rotation frames.
                 double relativeRotation;
 
                 if (o == 0)
-                {
                     relativeRotation = orders[o].Rotation - InitialRotation;
-                    if (orders[o].Rotation < 0)
-                        relativeRotation += Math.PI * 2;
-                }
                 else
-                {
                     relativeRotation = orders[o].Rotation - orders[o - 1].Rotation;
-                    if (orders[o].Rotation < 0)
-                        relativeRotation += Math.PI * 2;
-                    if (orders[o - 1].Rotation < 0)
-                        relativeRotation += Math.PI*2;
-                }
-
 
                 int partCount = (int)(Math.Abs(relativeRotation) * partsPerRadian);
 
@@ -98,16 +88,89 @@ namespace SRL.Main.ViewModel
                     Position = currentPosition,
                     Rotation = orders[o].Rotation
                 });
+
+                // Move frames.
+                Point start = o == 0 ? Startpoint : orders[o - 1].Destination;
+                Point end = orders[o].Destination;
+
+                double dx = end.X - start.X;
+                double dy = end.Y - start.Y;
+
+                currentAngle = frames.Peek().Rotation;
+                currentPosition = frames.Peek().Position;
+                if (Math.Abs(dx) < 0.5)
+                {
+                    for (double y = 1; y <= dy; y++)
+                    {
+                        frames.Push(new Frame
+                        {
+                            Position = new Point(currentPosition.X, currentPosition.Y + y),
+                            Rotation = currentAngle,
+                        });
+                    }
+                }
+                else if (Math.Abs(dy) < 0.5)
+                {
+                    for (double x = 1; x <= dx; x++)
+                    {
+                        frames.Push(new Frame
+                        {
+                            Position = new Point(currentPosition.X + x, currentPosition.Y),
+                            Rotation = currentAngle,
+                        });
+                    }
+                }
+                else
+                {
+                    double xStep = dx > 0 ? 1 : -1;
+                    double yStep = dy > 0 ? 1 : -1;
+
+                    // Based on Bresenham's line algorithm.
+                    if (Math.Abs(dx) > Math.Abs(dy))
+                    {
+                        double error = Math.Abs(dx/2) - Math.Abs(dy);
+                        for (double x = xStep, y = 0; x <= dx; x += xStep)
+                        {
+                            if (error < 0)
+                            {
+                                y += yStep;
+                                error += Math.Abs(dx);
+                            }
+                            else
+                                error -= Math.Abs(dy);
+
+                            frames.Push(new Frame
+                            {
+                                Position = new Point(currentPosition.X + x, currentPosition.Y + y),
+                                Rotation = currentAngle,
+                            });
+                        }
+                    }
+                    else
+                    {
+                        double error = Math.Abs(dy / 2) - Math.Abs(dx);
+                        for (double y = yStep, x = 0; y <= dx; y += yStep)
+                        {
+                            if (error < 0)
+                            {
+                                x += xStep;
+                                error += Math.Abs(dy);
+                            }
+                            else
+                                error -= Math.Abs(dx);
+
+                            frames.Push(new Frame
+                            {
+                                Position = new Point(currentPosition.X + x, currentPosition.Y + y),
+                                Rotation = currentAngle,
+                            });
+                        }
+                    }
+                }
+
             }
-
-
-
-
-
-
-
-
-
+            _frames = frames.ToArray();
+            _frames = (Frame[])_frames.Reverse();
         }
     }
 }
