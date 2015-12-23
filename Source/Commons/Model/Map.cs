676d5e56f7239fdc5698bc68a6df3047edfb84b3
kowalikw <kowalikw.mini@gmail.com>
2015-12-23 15:31:16 +0100

    ISaveable interface

--- Source/Commons/Model/Map.cs
+++ Source/Commons/Model/Map.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace SRL.Commons.Model
 {
-    public class Map : IXmlSerializable
+    [XmlRoot(ElementName = "svg", Namespace = "http://www.w3.org/2000/svg")]
+    public class Map : IXmlSerializable, ISaveable
     {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public string Type { get; set; }
+
         public List<Polygon> Obstacles { get; }
 
         public Map()
@@ -26,7 +35,49 @@ namespace SRL.Commons.Model
 
         public void WriteXml(XmlWriter writer)
         {
-            throw new System.NotImplementedException();
+            writer.WriteStartAttribute("width");
+            writer.WriteValue(Width);
+            writer.WriteEndAttribute();
+
+            writer.WriteStartAttribute("height");
+            writer.WriteValue(Height);
+            writer.WriteEndAttribute();
+
+            writer.WriteStartAttribute("type");
+            writer.WriteValue(Type);
+            writer.WriteEndAttribute();
+
+            writer.WriteStartElement("rect");
+
+            writer.WriteStartAttribute("width");
+            writer.WriteValue(Width);
+            writer.WriteEndAttribute();
+
+            writer.WriteStartAttribute("height");
+            writer.WriteValue(Height);
+            writer.WriteEndAttribute();
+
+            writer.WriteStartAttribute("fill");
+            writer.WriteValue("rgb(1, 47, 135)");
+            writer.WriteEndAttribute();
+
+            writer.WriteEndElement();
+
+            foreach (Polygon polygon in Obstacles)
+            {
+                string points = "";
+
+                foreach (Point point in polygon.Vertices)
+                    points += point.X + point.Y + " ";
+
+                writer.WriteStartElement("polygon");
+
+                
+
+                writer.WriteEndElement();
+            }
+
+            //throw new System.NotImplementedException();
         }
     }
 }
