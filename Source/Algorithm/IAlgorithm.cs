c8a637de42be82898ca9bf79d5a263c39688ed86
Konrad Miśkiewicz <miskiewicz.konrad@gmail.com>
2015-12-24 11:58:25 +0100

    Mock, RotatePoint, IAlgorithm

---
+++ Source/Algorithm/IAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SRL.Commons.Model;
+using System.Windows;
+
+
+namespace SRL.Algorithm
+{
+    interface IAlgorithm
+    {
+        List<Order> GetPath(Map map, Vehicle vehicle, Point start, Point end, double vehicleRotation, int angleDensity);
+    }
+}
--- Source/Commons/Utilities/GeometryHelper.cs
+++ Source/Commons/Utilities/GeometryHelper.cs
@@ -49,5 +49,16 @@ namespace SRL.Commons.Utilities
         {
             return Math.Sqrt(Math.Pow((p.X - q.X), 2) + Math.Pow((p.Y - q.Y), 2));
         }
+
+        public static Point RotatePoint(Point pointToRotate, Point centerPoint, double angleInRadians)
+        {
+            double cosTheta = Math.Cos(angleInRadians);
+            double sinTheta = Math.Sin(angleInRadians);
+            return new Point(
+                    (cosTheta * (pointToRotate.X - centerPoint.X) -
+                    sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X),
+                    (sinTheta * (pointToRotate.X - centerPoint.X) +
+                    cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y));
+        }
     }
 }
