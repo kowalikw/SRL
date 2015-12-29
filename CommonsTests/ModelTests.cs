15810aeba880067ec6b0456d6eca3be54bdb1333
kowalikw <kowalikw.mini@gmail.com>
2015-12-29 11:35:39 +0100

    CommonsTests project added

---
+++ CommonsTests/ModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonsTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void VehicleWriteXmlTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void VehicleReadXmlTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapWriteXmlTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapReadXmlTest()
+        {
+
+        }
+    }
+}
