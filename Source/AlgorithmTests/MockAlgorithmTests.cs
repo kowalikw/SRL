44f85a85fa2654e253dc978b91883b9951fbf17a
kowalikw <kowalikw.mini@gmail.com>
2016-01-04 14:43:17 +0100

    Build path fixes. AlgorithmTests project added.

---
+++ Source/AlgorithmTests/MockAlgorithmTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class MockAlgorithmTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
